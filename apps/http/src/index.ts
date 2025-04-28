9fb2e13c912026072f4a08815c1838843a867719
Vagarth <vagarth419@gmail.com>
2025-04-28 23:09:04 +0530

    chore: add typescript config as dev dependency

--- apps/http/src/index.ts
+++ apps/http/src/index.ts
@@ -2,6 +2,23 @@ import express from "express";
 
 const app = express();
 
+app.post("/signup", (req, res) => {
+  const { email, password } = req.body;
+  res.send("Hello World");
+});
+
+app.post("/signin", (req, res) => {
+  const { email, password } = req.body;
+  res.send("Hello World");
+});
+
+app.post("/create-room", (req, res) => {
+  const { roomId } = req.body;
+  res.send("Hello World");
+});
+
+
+
 app.listen(3001, () => {
   console.log("Server is running on port 3001");
 });
