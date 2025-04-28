300a3eba98a0a680c1e931b6ae44ce2909fcadc8
Vagarth <vagarth419@gmail.com>
2025-04-28 23:33:28 +0530

    feat: add try/catch error handling to the middleware

--- apps/http/src/index.ts
+++ apps/http/src/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import middleware from "./middleware";
 
 const app = express();
 
 app.post("/signup", (req, res) => {
-  
+  res.json({
+    userId: 1,
+  })
 });
 
 app.post("/signin", (req, res) => {
@@ -14,8 +17,10 @@ app.post("/signin", (req, res) => {
   res.json({ token });
 });
 
-app.post("/create-room", (req, res) => {
-  ;
+app.post("/create-room", middleware, (req, res) => {
+  res.json({
+    roomId: 1,
+  })
 });
 
 app.listen(3001, () => {
--- apps/http/src/middleware.ts
+++ apps/http/src/middleware.ts
@@ -9,19 +9,23 @@ declare global {
   }
 }
 
-const middleware = (req: Request, res: Response, next: NextFunction) => {
+const middleware = (req: Request, res: Response, next: NextFunction): void => {
   const token = req.header('Authorization') ?? "";
   if (!token) {
-    return res.status(401).json({ message: "Unauthorized" });
+    res.status(401).json({ message: "Unauthorized" });
+    return;
   }
-  const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as JwtPayload;
-  if (decoded.userId) {
-    req.userId = decoded.userId;
-    next();
-  } else {
-    return res.status(401).json({ message: "Unauthorized" });
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as JwtPayload;
+    if (decoded.userId) {
+      req.userId = decoded.userId;
+      next();
+    } else {
+      res.status(401).json({ message: "Unauthorized" });
+    }
+  } catch (error) {
+    res.status(401).json({ message: "Invalid token" });
   }
 };
 
 export default middleware;
-
