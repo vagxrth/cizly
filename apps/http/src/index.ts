794dc1064a8dd38c7c8406c62e3d98f65875d20b
Vagarth <vagarth419@gmail.com>
2025-04-28 23:23:19 +0530

    feat: create a demo signin endpoint

--- apps/http/src/index.ts
+++ apps/http/src/index.ts
@@ -1,23 +1,22 @@
 import express from "express";
+import jwt from "jsonwebtoken";
 
 const app = express();
 
 app.post("/signup", (req, res) => {
-  const { email, password } = req.body;
-  res.send("Hello World");
+  
 });
 
 app.post("/signin", (req, res) => {
-  const { email, password } = req.body;
-  res.send("Hello World");
+  const userId = 1;
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET as string);
+  console.log(process.env.JWT_SECRET);
+  res.json({ token });
 });
 
 app.post("/create-room", (req, res) => {
-  const { roomId } = req.body;
-  res.send("Hello World");
+  ;
 });
-
-
 
 app.listen(3001, () => {
   console.log("Server is running on port 3001");
